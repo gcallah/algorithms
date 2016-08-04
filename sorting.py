1aca5e42c7fdf1468d770add92e554bf3f151623
gcallah <gcallah@mac.com>
2016-08-03 20:24:17 -0400

    Insert sort working.

--- sorting.py
+++ sorting.py
@@ -4,6 +4,10 @@ Intro to Algorithms (Cormen et al.)
 The aim here is not efficient Python implementations (we'd just call
 the native sort if we wanted that) but to duplicate the pseudo-code
 in the book as closely as possible.
+Also, since the goal is to bring students to see how the algorithm
+works, there are print statements placed at key points in the code.
+I have been helped by code from http://interactivepython.org/
+in creating these examples.
 """
 
 import sys
@@ -28,6 +32,10 @@ def merge_sort(l):
         return merge(left, right)
 
 def merge(left, right):
+    """
+    Helper function for merge_sort: this actually
+    merges the split and already sorted sub-lists.
+    """
     sorted = []
     left.append(SENTINEL)
     right.append(SENTINEL)
@@ -35,11 +43,36 @@ def merge(left, right):
     j = 0
     for k in range(len(right) + len(left) - 2):
         if left[i] <= right[j]:
-            print("Appending " + str(left[i]))
+            print("Appending " + str(left[i])
+                    + " to " + str(sorted))
             sorted.append(left[i])
             i += 1
         else:
-            print("Appending " + str(right[j]))
+            print("Appending " + str(right[j])
+                    + " to " + str(sorted))
             sorted.append(right[j])
             j += 1
     return sorted
+
+
+def insert_sort(l):
+    for j in range(1, len(l)):
+        key = l[j]
+        print("key = " + str(key))
+        i = j - 1
+        while i >= 0 and l[i] > key:
+            print("moving " + str(l[i]) + " forward one.")
+            l[i + 1] = l[i]
+            i -= 1
+        l[i + 1] = key
+    print(l)
+
+
+def bubble_sort(l):
+    for i in range(0, len(l) - 1):
+        for j in range(len(l) - 1, i + 1, -1):
+            if l[j] < l[j - 1]:
+                temp = l[j]
+                l[j] = l[j - 1]
+                l[j - 1] = temp
+    print(l)
