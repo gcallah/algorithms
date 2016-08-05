84cc6383025c42789771f8abf0d63285ea17054c
gcallah <gcallah@mac.com>
2016-08-04 21:16:50 -0400

    Added quick sort to the sorting menagerie.

--- sorting.py
+++ sorting.py
@@ -8,6 +8,9 @@ Also, since the goal is to bring students to see how the algorithm
 works, there are print statements placed at key points in the code.
 I have been helped by code from http://interactivepython.org/
 in creating these examples.
+The performance of each sort is stated in the docstring, and
+loop invariants are expressed as assert statements when they
+are not too complex.
 """
 
 import sys
@@ -15,9 +18,23 @@ import sys
 SENTINEL = sys.maxsize
 
 
+def swap(l, i, j):
+    """
+    Since several sort algorithms need to swap list
+    elements, we provide a swap function.
+    l: the list
+    i, j: the indices of the elements to swap.
+    """
+    temp = l[i]
+    l[i] = l[j]
+    l[j] = temp
+
+
 def merge_sort(l):
     """
-    l is the list to sort
+    l: the list to sort
+    Returns: a sorted list
+    Performance: Θ(n * log n) 
     """
     length = len(l)
     if length <= 1:
@@ -35,6 +52,8 @@ def merge(left, right):
     """
     Helper function for merge_sort: this actually
     merges the split and already sorted sub-lists.
+    left: a sorted list
+    right: a sorted list
     """
     sorted = []
     left.append(SENTINEL)
@@ -56,6 +75,11 @@ def merge(left, right):
 
 
 def insert_sort(l):
+    """
+    l: the list to sort
+    Returns: a sorted list.
+    Performance: Θ(n**2) 
+    """
     for j in range(1, len(l)):
         key = l[j]
         print("key = " + str(key))
@@ -65,16 +89,64 @@ def insert_sort(l):
             l[i + 1] = l[i]
             i -= 1
         l[i + 1] = key
-    print(l)
+    return l
 
 
 def bubble_sort(l):
+    """
+    l: the list to sort
+    Returns: a sorted list.
+    Performance: Θ(n**2) 
+    """
     for i in range(0, len(l) - 1):
         for j in range(len(l) - 1, i, -1):
             if l[j] < l[j - 1]:
                 print("Swapping " + str(l[j]) + " and "
                         + str(l[j - 1]))
-                temp = l[j]
-                l[j] = l[j - 1]
-                l[j - 1] = temp
+                swap(l, j, j - 1)
     print(l)
+
+
+def quick_sort(l, p=None, r=None):
+    """
+    l: the list to sort
+    p: the first index in a partition
+    r: the last index in a partition
+    Returns: a sorted list.
+    Performance:
+        Worst case: Θ(n**2) 
+        Expected case: Θ(n * log n) 
+        Sorts in place.
+    """
+    if p is None:
+        p = 0
+    if r is None:
+        r = len(l) - 1
+    if p < r:
+        q = partition(l, p, r)
+        print("Partitioning list at index " + str(q))
+        print("The list is now: " + str(l))
+        quick_sort(l, p, q - 1)
+        quick_sort(l, q + 1, r)
+
+def partition(l, p, r):
+    """
+    Helper function for quick_sort.
+    Returns: the new partition index.
+    """
+    x = l[r]
+    print("Our pivot element x = " + str(x))
+    i = p - 1
+    for j in range(p, r):
+        if l[j] <= x:
+            i += 1
+            print("i = " + str(i) + " and j = " + str(j))
+            if i != j:
+                print("Swapping elements " + str(l[i]) + " and "
+                     + str(l[j]))
+                swap(l, i, j)
+    if (i + 1) != r:
+        swap(l, i + 1, r)
+        print("Swapping elements " + str(l[i + 1]) + " and "
+              + str(l[r]))
+    return i + 1
