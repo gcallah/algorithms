1d943ad2de45f4cadbaede2381c5f628d4b08834
gcallah <gcallah@mac.com>
2016-08-05 20:15:18 -0400

    Beginning work on heaps.

---
+++ heap.py
@@ -0,0 +1,33 @@
+"""
+Because Introduction to Algorithms (Cormen et al.) include priority queues in
+their discussion of heap sort, I have made a separate heap.py file,
+as I do not want to clutter the sorting.py file too much.
+so here we have routines to establish and maintain the heap, as well as
+heapsort itself and a priority queue function.
+As in sorting.pY, the intention is to closely duplicate the pseudocode from
+the book, and again, print statements will be interleaved into the code to help
+students understand how the algorithms works.
+We will allow max or min heaps based on a passed comparison operator.
+This file contains:
+    heapify()
+    build_heap()
+    heapsort()
+    heap_insert()
+    heap_extract_max()
+    heap_increase_key()
+    heap_maximum()
+"""
+
+import sorting   # so we don't have to duplicate swap!
+
+
+def heapify(l, i):
+    pass
+
+
+def build__heap(l):
+    pass
+
+
+def heapsort(l):
+    pass
