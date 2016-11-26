464eb8b980230f62b2c8aa5a838dbe0390cd6d62
gcallah <gcallah@mac.com>
2016-11-26 00:37:34 -0500

    Developing dynamic programming.

--- python/dynamic.py
+++ python/dynamic.py
@@ -14,27 +14,33 @@ This file contains:
     cut_rod()
     memo_cut_rod()
     memo_cut_rod_aux()
+    bottom_up_cut_rod():
 """
 
 NEG_REV = -1
 calls = 0
 
-def cut_rod(prices):
+def cut_rod(prices, n):
     """
         Args:
             prices: an array of prices 0..n for rods of various lengths
-            CLRS passes n, but since it is just len(p)...
+            n: length of rod
         Returns:
             max_rev: the maximum revenue possible from cutting a rod of length n.
-            Note: This is a *terrible* thing to return in a real world
+            Note: This is not a good value to return in a real world
             application: what we would really like returned is what 
             cuts to make to *achieve* that maximum revenue!
+            Our textbook handles this in the extended version of bottom up rod
+            cutting.
             Note2: CLRS calls variable q.
 
     """
+    if n > len(prices):
+        print("Not enough prices for rod of len " + str(n))
+        return None
+
     global calls
     calls += 1
-    n = len(prices)
     if n == 0:
         return 0
     max_rev = NEG_REV  # CLRS uses negative infinity, but that hardly seems
@@ -42,40 +48,39 @@ def cut_rod(prices):
                        # best to just go out of business immediately!
     for i in range(0, n):
         max_rev = max(max_rev,
-                      prices[i] + cut_rod(prices[:n - i - 1]))
+                      prices[i] + cut_rod(prices[:n - i - 1], n - i - 1))
         print("Handling rod of len " + str(n) + "; max revenue currently is " +
               str(max_rev))
     print("Calls = " + str(calls))
     return max_rev
 
 
-def memo_cut_rod(prices):
+def memo_cut_rod(prices, n):
     """
         Args:
             prices: an array of prices 0..n for rods of various lengths
-            CLRS passes n, but since it is just len(prices)...
+            n: length of rod
         Returns:
-            max_rev: maximum revenua
+            max_rev: maximum revenue possible
     """
-    n = len(prices)
     if n == 0:
         return 0
     max_rev = NEG_REV
     revs = [NEG_REV for i in range(n)]
-    return memo_cut_rod_aux(prices, revs)
+    return memo_cut_rod_aux(prices, n, revs)
 
 
-def memo_cut_rod_aux(prices, revs):
+def memo_cut_rod_aux(prices, n, revs):
     """
         Args:
             prices: an array of prices 0..n for rods of various lengths
             revs: revenues previously calculated
+            n: length of rod
         Returns:
-            max_rev: maximum revenua
+            max_rev: maximum revenue
     """
     global calls
     max_rev = NEG_REV
-    n = len(prices)
     if revs[n - 1] >= 0:
         print("Using memo value with n = " + str(n)
               + " and revs[n - 1] = "
@@ -88,21 +93,95 @@ def memo_cut_rod_aux(prices, revs):
         for i in range(0, n):
             max_rev = max(max_rev,
                           prices[i]
-                          + memo_cut_rod_aux(prices[:n - i - 1], revs))
+                          + memo_cut_rod_aux(prices[:n - i - 1],
+                                             n - i - 1,
+                                             revs))
             print("Handling rod of len " + str(n)
                   + "; max revenue currently is " +
                   str(max_rev))
 
-#    print("n = " + str(n) + "; setting revs[n - 1] = "
-#          + str(revs[n - 1]))
     revs[n - 1] = max_rev
     print("Calls = " + str(calls))
     return max_rev
 
 
-# some price arrays for testing:
+def bottom_up_cut_rod(prices, n):
+    """
+        Args:
+            prices: an array of prices for different rod lengths
+            n: length of rod
+        Returns:
+            max_rev: maximum revenue possible
+    """
+    if n > len(prices):
+        print("Not enough prices for rod of len " + str(n))
+        return None
+
+    max_rev = NEG_REV
+    revs = [0 for i in range(n + 1)]  # CLRS only initializes first elem,
+                                      # but no harm doing all
+    for j in range(1, n + 1):  # 0th element holds a 0
+        max_rev = NEG_REV
+        for i in range(j):
+            max_rev = max(max_rev, prices[i] + revs[j - i - 1])
+
+        print("Maximum revenue calculated at step " + str(j)
+              + " = " + str(max_rev))
+        revs[j] = max_rev
+
+    return revs[n]
+
+
+def ext_bottom_up_cut_rod(prices, n):
+    """
+        This calculates the maximum revenue, but also returns a list of what
+        cuts generate it.
+        Args:
+            prices: an array of prices for different rod lengths
+            n: length of rod
+        Returns:
+            max_rev: maximum revenue possible
+    """
+    if n > len(prices):
+        print("Not enough prices for rod of len " + str(n))
+        return None
+
+    max_rev = NEG_REV
+    revs = [0 for i in range(n + 1)]  # CLRS only initializes first elem,
+                                      # but no harm doing all
+    cuts = [0 for i in range(n)]
+    for j in range(1, n):  # 0th element holds a 0
+        max_rev = NEG_REV
+        for i in range(j - 1):
+            if max_rev < prices[i] + revs[j - i - 1]:
+                max_rev = prices[i] + revs[j - i - 1]
+                cuts[j] = i + 1
+
+        revs[j] = max_rev
+
+    return (revs, cuts)
+
+
+def print_cut_rod(prices, n):
+    """
+        Args:
+            prices: price array
+            n: length of rod
+        Returns:
+            None
+            Prints optimal cuts
+    """
+    (revs, cuts) = ext_bottom_up_cut_rod(prices, n)
+    while n > 0:
+        print("n = " + str(n))
+        print("Make a cut at: " + str(cuts[n - 1]))
+        n = n - cuts[n - 1]
+
+
+# some price arrays for testing rod cutting:
 p1 = [3]
 p2 = [3, 7]
 p3 = [3, 7, 9]
 p4 = [3, 7, 9, 12]
 p7 = [3, 5, 10, 11, 14, 17, 20]
+p10 = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
