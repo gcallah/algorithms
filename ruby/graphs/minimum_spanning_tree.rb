74a6c097ea9f48aa4198ad9505767d2770d05d3a
nandukalidindi <nandukalidindi@gmail.com>
2016-12-03 01:19:09 -0500

    Preparation for minimum spanning tree algorithms

---
+++ ruby/graphs/minimum_spanning_tree.rb
@@ -0,0 +1,16 @@
+module Graphs
+  class MinimumSpanningTree
+    class << self
+      def MST_generic
+
+      end
+
+      def MST_kruskal
+
+      end
+
+      def MST_prim
+      end
+    end
+  end
+end
