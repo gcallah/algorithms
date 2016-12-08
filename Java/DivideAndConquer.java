63af9485c9c3dc4ced05cae1ba7289d1e100512b
xuzebin <zebinxu7@gmail.com>
2016-12-08 16:34:49 -0500

    Fix up indentation

--- Java/DivideAndConquer.java
+++ Java/DivideAndConquer.java
@@ -8,73 +8,73 @@ import java.util.Arrays;
 public class DivideAndConquer {
  
     public static void main(String[] args) {
-	DivideAndConquer dac = new DivideAndConquer();
-	int[] a = {2, -1, 3, -2};
-	Tuple max = dac.findMaximumSubarray(a, 0, a.length - 1);
-	System.out.println(Arrays.toString(a));
-	System.out.printf("max sum: %d, from %d to %d%n", max.sum, max.low, max.high);
+        DivideAndConquer dac = new DivideAndConquer();
+        int[] a = {2, -1, 3, -2};
+        Tuple max = dac.findMaximumSubarray(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+        System.out.printf("max sum: %d, from %d to %d%n", max.sum, max.low, max.high);
     }
 
     private static class Tuple {
-	int low;
-	int high;
-	int sum;
+        int low;
+        int high;
+        int sum;
 
-	Tuple(int low, int high, int sum) {
-	    this.low = low;
-	    this.high = high;
-	    this.sum = sum;
-	}
+        Tuple(int low, int high, int sum) {
+            this.low = low;
+            this.high = high;
+            this.sum = sum;
+        }
     }
 
     private Tuple findMaxCrossingSubarray(int[] a, int low, int mid, int high) {
-	if (a == null || a.length == 0) {
-	    throw new IllegalArgumentException("Array null or empty");
-	}
-	int leftSum = Integer.MIN_VALUE;
-	int sum = 0;
-	int maxLeft = mid;
-	for (int i = mid; i >= low; --i) {
-	    sum += a[i];
-	    if (sum > leftSum) {
-		leftSum = sum;
-		maxLeft = i;
-	    }
-	}
+        if (a == null || a.length == 0) {
+            throw new IllegalArgumentException("Array null or empty");
+        }
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int maxLeft = mid;
+        for (int i = mid; i >= low; --i) {
+            sum += a[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
 
-	int rightSum = Integer.MIN_VALUE;
-	sum = 0;
-	int maxRight = mid + 1;
-	for (int j = mid + 1; j <= high; ++j) {
-	    sum += a[j];
-	    if (sum > rightSum) {
-		rightSum = sum;
-		maxRight = j;
-	    }
-	}
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        int maxRight = mid + 1;
+        for (int j = mid + 1; j <= high; ++j) {
+            sum += a[j];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRight = j;
+            }
+        }
 	
-	return new Tuple(maxLeft, maxRight, leftSum + rightSum);
+        return new Tuple(maxLeft, maxRight, leftSum + rightSum);
     }
 
     public Tuple findMaximumSubarray(int[] a, int low, int high) {
-	if (a == null || a.length == 0) {
-	    throw new IllegalArgumentException("Array null or empty");
-	}
-	if (low == high) {
-	    return new Tuple(low, high, a[low]);
-	} else {
-	    int mid = (low + high) >>> 1;
-	    Tuple left = findMaximumSubarray(a, low, mid);
-	    Tuple right = findMaximumSubarray(a, mid + 1, high);
-	    Tuple cross = findMaxCrossingSubarray(a, low, mid, high);
+        if (a == null || a.length == 0) {
+            throw new IllegalArgumentException("Array null or empty");
+        }
+        if (low == high) {
+            return new Tuple(low, high, a[low]);
+        } else {
+            int mid = (low + high) >>> 1;
+            Tuple left = findMaximumSubarray(a, low, mid);
+            Tuple right = findMaximumSubarray(a, mid + 1, high);
+            Tuple cross = findMaxCrossingSubarray(a, low, mid, high);
 	    
-	    if (left.sum >= right.sum && left.sum >= cross.sum) {
-		return left;
-	    } else if (right.sum >= left.sum && right.sum >= cross.sum) {
-		return right;
-	    } else {
-		return cross;
-	    }
-	}
+            if (left.sum >= right.sum && left.sum >= cross.sum) {
+                return left;
+            } else if (right.sum >= left.sum && right.sum >= cross.sum) {
+                return right;
+            } else {
+                return cross;
+            }
+        }
     }
 }
--- Java/graphs/AdjacencyList.java
+++ Java/graphs/AdjacencyList.java
@@ -33,49 +33,49 @@ import java.util.ArrayList;
 public class AdjacencyList {
 
     public static List<Vertex> build(final int[][] adjacencyList, int numberOfVertex) {
-	List<Vertex> vertices = new ArrayList<>(numberOfVertex);
-	Vertex[] vs = new Vertex[numberOfVertex];
-	for (int i = 0; i < numberOfVertex; ++i) {
-	    vs[i] = new Vertex(i);
-	}
+        List<Vertex> vertices = new ArrayList<>(numberOfVertex);
+        Vertex[] vs = new Vertex[numberOfVertex];
+        for (int i = 0; i < numberOfVertex; ++i) {
+            vs[i] = new Vertex(i);
+        }
 	
-	int k = 0;
-	for (int i = 0; i < numberOfVertex; ++i) {
-	    if (k < adjacencyList.length) {
-		int firstVertexId = adjacencyList[k][0];
-		if (i < firstVertexId) {
-		    vertices.add(vs[i]);//Add vertices that has no neighbor in the List
-		} else if (i == firstVertexId) {
-		    Vertex v = vs[i];
-		    for (int j = 1; j < adjacencyList[k].length; ++j) {
-			v.neighbors.add(vs[adjacencyList[k][j]]);
-		    }
-		    vertices.add(v);
-		    k++;
-		} else {
-		    throw new IllegalArgumentException("The adjacency lists do not matched the expected form");
-		}		
-	    } else {
-		vertices.add(vs[i]);
-	    }
-	}
-	return vertices;
+        int k = 0;
+        for (int i = 0; i < numberOfVertex; ++i) {
+            if (k < adjacencyList.length) {
+                int firstVertexId = adjacencyList[k][0];
+                if (i < firstVertexId) {
+                    vertices.add(vs[i]);//Add vertices that has no neighbor in the List
+                } else if (i == firstVertexId) {
+                    Vertex v = vs[i];
+                    for (int j = 1; j < adjacencyList[k].length; ++j) {
+                        v.neighbors.add(vs[adjacencyList[k][j]]);
+                    }
+                    vertices.add(v);
+                    k++;
+                } else {
+                    throw new IllegalArgumentException("The adjacency lists do not matched the expected form");
+                }		
+            } else {
+                vertices.add(vs[i]);
+            }
+        }
+        return vertices;
     }
 
     public static void print(List<Vertex> vertices) {
-	System.out.println("Adjacency Lists representation of the graph:");
-	for (Vertex v : vertices) {
-	    System.out.printf("%s", v);
-	    if (v.neighbors.size() > 0) {
-		System.out.printf(" -> ");
-		for (int i = 0; i < v.neighbors.size() - 1; ++i) {
-		    System.out.printf("%s -> ", v.neighbors.get(i));
-		}
-		System.out.printf("%s%n", v.neighbors.get(v.neighbors.size() - 1));
-	    } else {
-		System.out.printf("%n");
-	    }
-	}
-	System.out.println("----------------------");
+        System.out.println("Adjacency Lists representation of the graph:");
+        for (Vertex v : vertices) {
+            System.out.printf("%s", v);
+            if (v.neighbors.size() > 0) {
+                System.out.printf(" -> ");
+                for (int i = 0; i < v.neighbors.size() - 1; ++i) {
+                    System.out.printf("%s -> ", v.neighbors.get(i));
+                }
+                System.out.printf("%s%n", v.neighbors.get(v.neighbors.size() - 1));
+            } else {
+                System.out.printf("%n");
+            }
+        }
+        System.out.println("----------------------");
     }
 }
