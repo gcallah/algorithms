58c21df944c5eaa9d397c96d3b0fcae8e5ebe8d6
nandukalidindi <nandukalidindi@gmail.com>
2016-12-09 01:47:59 -0500

    Add recorder memoized cut rod version from the assignment

--- ruby/dynamic_programming/rod_cutting.rb
+++ ruby/dynamic_programming/rod_cutting.rb
@@ -61,6 +61,87 @@ module DynamicProgramming
         q
       end
 
+      # Internal: Extension method to memoized_cut_rod to return a tuple of
+      #           max revenues and array consisting of optimal index cuts
+      #           Recursive Memoization Technique
+      #
+      # p - Price list to cut a rod of certain length
+      # n - Length of rod to be cut
+      #
+      # Examples
+      #   p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+      #   memoized_cut_rod_with_recorder(p, 9)
+      #   => [
+      #        [1, 5, 8, 10, 13, 17, 18, 22, 25, 30],
+      #        [1, 2, 3, 2, 2, 6, 1, 2, 3, 10]
+      #      ]
+      def memoized_cut_rod_with_recorder(p, n)
+        r = n.times.map { |x| -Float::INFINITY }
+        s = n.times.map { |x| 0 }
+        memoized_cut_rod_aux_with_recorder(p, n, r, s)
+      end
+
+      # Internal: Auxiliary extenstion method to memoized_cut_rod_aux to
+      #           calculate the optimal revenue array and the cut index array
+      #
+      # p - Price list to cut a rod of certain length
+      # n - Length of rod to be cut
+      # r - Optimal solution array - NOTE: To be initialized to -INFINITY array
+      # s - Cut index array - NOTE: To be initialized to a zero array by callee
+      #
+      # Examples
+      #   p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+      #   memoized_cut_rod_aux(p, 9, [-Float::INFINITY, ..], [0, 0, ..])
+      #   => [
+      #        [1, 5, 8, 10, 13, 17, 18, 22, 25, 30],
+      #        [1, 2, 3, 2, 2, 6, 1, 2, 3, 10]
+      #      ]
+      def memoized_cut_rod_aux_with_recorder(p, n, r, s)
+        return r[n-1] if r[n-1] >= 0
+        index = 0
+        if n == 0
+          index = 0
+          q = 0
+        else
+          q = -Float::INFINITY
+          (0..n-1).each do |i|
+            old_optimal_revenue = memoized_cut_rod_aux_with_recorder(p, n-i-1, r, s)
+            if old_optimal_revenue.class == Array
+              old_optimal_revenue = old_optimal_revenue.first[n-i-2]
+            end
+
+            if p[i] + old_optimal_revenue > q
+              q = p[i] + old_optimal_revenue
+              index = i
+            end
+          end
+        end
+        s[n-1] = index+1
+        r[n-1] = q
+        [r, s]
+      end
+
+      # Internal: Auxiliary method to memoized_cut_rod_with_recorder to print
+      #           the rod-lengths to be cut in order to maximize the revenue
+      #
+      # s - Optimal cut index array obtained from memoized_cut_rod_with_recorder
+      # n - Length of rod to be cut
+      #
+      # Examples
+      #   s = [1, 2, 3, 2, 2, 6, 1, 2, 3, 10]
+      #   cut_locations(s, 7)
+      #   => [1, 6]
+      def cut_locations(s, n)
+        breakable = false
+        cut_list = []
+        while true
+          cut_list << s[n-1]
+          n = n - s[n-1]
+          break if n == 0
+        end
+        cut_list
+      end
+
       # Internal: Method to calculate the maximum and optimal solution for cutting a
       #           rod of a particular length
       #           Recursive Memoization Technique. RUBIFIED METHOD!
--- ruby/dynamic_programming/unit_tests/rod_cutting_test.rb
+++ ruby/dynamic_programming/unit_tests/rod_cutting_test.rb
@@ -8,7 +8,22 @@ end
 
 def memoized_cut_rod_test
   p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-  DynamicProgramming::RodCutting.cut_rod(p, 9) == 25
+  DynamicProgramming::RodCutting.memoized_cut_rod(p, 9) == 25
+end
+
+def memoized_cut_rod_with_recorder_test
+  p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+  result = DynamicProgramming::RodCutting.memoized_cut_rod_with_recorder(p, 10)
+  result == [
+              [1, 5, 8, 10, 13, 17, 18, 22, 25, 30],
+              [1, 2, 3, 2, 2, 6, 1, 2, 3, 10]
+            ]
+end
+
+def cut_locations_test
+  p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+  result = DynamicProgramming::RodCutting.memoized_cut_rod_with_recorder(p, 10)
+  DynamicProgramming::RodCutting.cut_locations(result.last, 7) == [1, 6]
 end
 
 def sleek_memoized_cut_rod_aux_test
@@ -28,6 +43,8 @@ end
 
 UnitTest.method('Cut rod', cut_rod_test)
 UnitTest.method('Memoized cut rod', memoized_cut_rod_test)
+UnitTest.method('Memoized cut rod Recorder', memoized_cut_rod_with_recorder_test)
+UnitTest.method('Cut locations', cut_locations_test)
 UnitTest.method('Sleek memoized cut rod', sleek_memoized_cut_rod_aux_test)
 UnitTest.method('Bottom up cut rod', bottom_up_cut_rod_test)
 UnitTest.method('Extended bottom up cut rod', extended_bottom_up_cut_rod_test)
