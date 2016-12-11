731aff61be9158f887caa9043bcef7fe2af737f4
nandukalidindi <nandukalidindi@gmail.com>
2016-12-10 20:14:00 -0500

    Add documentation for the completed knapsack implementations

--- ruby/greedy_algorithms/unit_tests/knapsack_test.rb
+++ ruby/greedy_algorithms/unit_tests/knapsack_test.rb
@@ -7,9 +7,20 @@ require_relative '../../unit_test'
 # w = [1, 3, 4, 5]
 # c = [1, 4, 5, 7]
 
-w = [70, 73, 77, 80, 82, 87, 90, 94, 9, 10, 11, 11, 11, 11, 120]
-c = [135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240]
-max_weight = 750
+@w = [70, 73, 77, 80, 82, 87, 90, 94, 9, 10, 11, 11, 11, 11, 120]
+@c = [135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240]
+@max_weight = 750
+
+def zero_one_recursive_test
+  GreedyAlgorithms::Knapsack.zero_one_recursive(@w, @c, @max_weight) == 2593
+end
+
+def zero_one_memoized_test
+  GreedyAlgorithms::Knapsack.zero_one_memoized(@w, @c, @max_weight).first == 2593
+end
 # p GreedyAlgorithms::Knapsack.zero_one_recursive(w, c, max_weight, w.length)
-p GreedyAlgorithms::Knapsack.zero_one_memoized(w, c, max_weight, w.length).first
-p GreedyAlgorithms::Knapsack.zero_one_bottom_up(w, c, max_weight)
+# p GreedyAlgorithms::Knapsack.zero_one_memoized(w, c, max_weight, w.length)
+# p GreedyAlgorithms::Knapsack.zero_one_bottom_up(w, c, max_weight)
+
+UnitTest.method('0-1 Knapsack Recursive', zero_one_recursive_test)
+UnitTest.method('0-1 Knapsack Memoized', zero_one_memoized_test)
