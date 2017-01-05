471b564790a4c7b996c677b30e6873c1d6f96a07
nandukalidindi <nandukalidindi@gmail.com>
2017-01-05 06:28:29 -0500

    Modify LCS unit test to Minitest

--- Rakefile
+++ Rakefile
@@ -1,5 +1,7 @@
 task :test do
   # DIVIDE AND CONQUER TESTS
   ruby("ruby/divide_and_conquer/unit_tests/find_max_subarray_test.rb")
-  ruby("ruby/divide_and_conquer/unit_tests/matrix_multiplication_test.rb")  
+  ruby("ruby/divide_and_conquer/unit_tests/matrix_multiplication_test.rb")
+
+  ruby("ruby/dynamic_programming/unit_tests/lcs_test.rb")  
 end
--- ruby/dynamic_programming/unit_tests/lcs_test.rb
+++ ruby/dynamic_programming/unit_tests/lcs_test.rb
@@ -1,17 +1,16 @@
-require_relative '../../unit_test'
 require_relative '../LCS'
+require 'minitest/autorun'
 
-def lcs_length_test
-  x = "ABCBDAB"
-  y = "BDCABA"
-  DynamicProgramming::LCS.LCS_length(x, y).first[x.length][y.length] == 4
+class LCSTest < Minitest::Test
+  def test_lcs_length
+    x = "ABCBDAB"
+    y = "BDCABA"
+    assert_equal(DynamicProgramming::LCS.LCS_length(x, y).first[x.length][y.length], 4)
+  end
+
+  def test_lcs_print
+    x = "ABCBDAB"
+    y = "BDCABA"
+    assert_equal(DynamicProgramming::LCS.print_LCS(DynamicProgramming::LCS.LCS_length(x, y).last, x, x.length, y.length), "BCBA")
+  end
 end
-
-def print_lcs_test
-  x = "ABCBDAB"
-  y = "BDCABA"
-  DynamicProgramming::LCS.print_LCS(DynamicProgramming::LCS.LCS_length(x, y).last, x, x.length, y.length) == "BCBA"
-end
-
-UnitTest.method('LCS Length', lcs_length_test)
-UnitTest.method('Print LCS', print_lcs_test)
