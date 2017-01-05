38a49b4008979873ddedd365df47a7cb1e818d16
nandukalidindi <nandukalidindi@gmail.com>
2017-01-05 06:34:13 -0500

    Move matrix_chain and optimal_bst to Minitest

--- ruby/dynamic_programming/unit_tests/matrix_chain_multiplication_test.rb
+++ ruby/dynamic_programming/unit_tests/matrix_chain_multiplication_test.rb
@@ -1,21 +1,19 @@
-require_relative '../../unit_test'
 require_relative '../matrix_chain_multiplication'
+require 'minitest/autorun'
 
-def matrix_chain_order_test
-  p = [30, 35, 15, 5, 10, 20, 25] # implies number of matrices = 6 = p.length - 1
-  DynamicProgramming::MatrixChain.matrix_chain_order(p).first[0][p.length-2] == 15125
+class MatrixChainMultiplicationTest < Minitest::Test
+  def test_matrix_chain_order
+    p = [30, 35, 15, 5, 10, 20, 25] # implies number of matrices = 6 = p.length - 1
+    assert_equal(DynamicProgramming::MatrixChain.matrix_chain_order(p).first[0][p.length-2], 15125)
+  end
+
+  def test_recursive_matrix_chain
+    p = [30, 35, 15, 5, 10, 20, 25] # implies number of matrices = 6 = p.length - 1
+    assert_equal(DynamicProgramming::MatrixChain.recursive_matrix_chain(p, 0, p.length-2), 15125)
+  end
+
+  def test_memoized_matrix_chain
+    p = [30, 35, 15, 5, 10, 20, 25]
+    assert_equal(DynamicProgramming::MatrixChain.memoized_matrix_chain(p), 15125)
+  end
 end
-
-def recursive_matrix_chain_test
-  p = [30, 35, 15, 5, 10, 20, 25] # implies number of matrices = 6 = p.length - 1
-  DynamicProgramming::MatrixChain.recursive_matrix_chain(p, 0, p.length-2) == 15125
-end
-
-def memoized_matrix_chain_test
-  p = [30, 35, 15, 5, 10, 20, 25]
-  DynamicProgramming::MatrixChain.memoized_matrix_chain(p) == 15125
-end
-
-UnitTest.method('Matrix Chain Order', matrix_chain_order_test)
-UnitTest.method('Recursive Matrix Chain', recursive_matrix_chain_test)
-UnitTest.method('Memoized Matrix Chain', memoized_matrix_chain_test)
--- ruby/dynamic_programming/unit_tests/optimal_binary_search_tree_test.rb
+++ ruby/dynamic_programming/unit_tests/optimal_binary_search_tree_test.rb
@@ -1,11 +1,11 @@
-require_relative '../../unit_test'
 require_relative '../optimal_binary_search_tree'
+require 'minitest/autorun'
 
-def optimal_bst_test
-  p = [0.00, 0.15, 0.10, 0.05, 0.10, 0.20]
-  q = [0.05, 0.10, 0.05, 0.05, 0.05, 0.10]
-  k = DynamicProgramming::OBST.optimal_bst(p, q, 5)
-  k.first[1][5] == 2.75
+class OptimalBinarySearchTreeTest < Minitest::Test
+  def test_optimal_bst
+    p = [0.00, 0.15, 0.10, 0.05, 0.10, 0.20]
+    q = [0.05, 0.10, 0.05, 0.05, 0.05, 0.10]
+    k = DynamicProgramming::OBST.optimal_bst(p, q, 5)
+    assert_equal(k.first[1][5], 2.75)
+  end
 end
-
-UnitTest::method('Optimal BST', optimal_bst_test)
