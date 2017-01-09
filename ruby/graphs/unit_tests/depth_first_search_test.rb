1f55ef7c350cd4d7f497d76a07fdca1374221e9f
nandukalidindi <nandukalidindi@gmail.com>
2017-01-09 11:09:53 -0500

    DFS test -> Minitest

--- ruby/graphs/unit_tests/depth_first_search_test.rb
+++ ruby/graphs/unit_tests/depth_first_search_test.rb
@@ -1,10 +1,10 @@
-require_relative '../../unit_test'
 require_relative './seed_graph'
 require_relative '../depth_first_search'
+require 'minitest/autorun'
 
-def dfs_test
-  graph = SeedGraph.undirected_graph
-  Graphs::DepthFirstSearch.DFS(graph, graph.vertices.last) == "FOUND!!"
+class DepthFirstSearchTest < Minitest::Test
+  def test_dfs
+    graph = SeedGraph.undirected_graph
+    assert_equal(Graphs::DepthFirstSearch.DFS(graph, graph.vertices.last), "FOUND!!")
+  end
 end
-
-UnitTest.method('DFS', dfs_test)
