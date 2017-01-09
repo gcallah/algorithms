61866cd4c0b0d409fdc62bb1e7e02324d942d934
nandukalidindi <nandukalidindi@gmail.com>
2017-01-09 11:10:19 -0500

    Kruskal test -> Minitest

--- ruby/graphs/unit_tests/minimum_spanning_tree_test.rb
+++ ruby/graphs/unit_tests/minimum_spanning_tree_test.rb
@@ -1,11 +1,11 @@
-require_relative '../../unit_test'
 require_relative './seed_graph'
 require_relative '../minimum_spanning_tree'
+require 'minitest/autorun'
 
-def mst_kruskal_test
-  graph = SeedGraph.kruskal_undirected_graph
-  Graphs::MinimumSpanningTree.MST_kruskal(graph).map { |x| [x.v1.key, x.v2.key] } ==
-  [["g", "h"], ["c", "i"], ["f", "g"], ["c", "f"], ["a", "b"], ["c", "d"], ["b", "c"], ["d", "e"]]
+class MinimumSpanningTreeTest < Minitest::Test
+  def test_mst_kruskal
+    graph = SeedGraph.kruskal_undirected_graph
+    assert_equal(Graphs::MinimumSpanningTree.MST_kruskal(graph).map { |x| [x.v1.key, x.v2.key] },
+    [["g", "h"], ["c", "i"], ["f", "g"], ["c", "f"], ["a", "b"], ["c", "d"], ["b", "c"], ["d", "e"]])
+  end
 end
-
-UnitTest::method('Kruskals MST', mst_kruskal_test)
