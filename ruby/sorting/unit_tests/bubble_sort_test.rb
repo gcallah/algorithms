0edb6894653e4f8cc7389d45d70c50955196034b
nandukalidindi <nandukalidindi@gmail.com>
2017-01-16 00:31:08 -0500

    Moved quick sort unit tests to Minitest

--- ruby/sorting/unit_tests/bubble_sort_test.rb
+++ ruby/sorting/unit_tests/bubble_sort_test.rb
@@ -5,7 +5,7 @@ class BubbleSortTest < Minitest::Test
   def test_bubble_sort
     sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     arr = [5, 3, 8, 7, 9, 6, 2, 4, 1]
-  	Sorting.bubble_sort(arr)
+  	Sorting::bubble_sort(arr)
   	assert_equal(arr, sorted_arr)
   end
 end
--- ruby/sorting/unit_tests/merge_sort_test.rb
+++ ruby/sorting/unit_tests/merge_sort_test.rb
@@ -5,7 +5,7 @@ class MergeSortTest < Minitest::Test
   def test_merge_sort
     sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     arr = [5, 3, 8, 7, 9, 6, 2, 4, 1]
-    Sorting.merge_sort(arr)
+    Sorting::merge_sort(arr)
     assert_equal(arr, sorted_arr)
   end
 
