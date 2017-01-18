9dfcaf539981fa185cb77b85d400dc7c769f8262
nandukalidindi <nandukalidindi@gmail.com>
2017-01-17 23:15:54 -0500

    You can fail now. Checking if the workaround is actually working

--- ruby/divide_and_conquer/unit_tests/find_max_subarray_test.rb
+++ ruby/divide_and_conquer/unit_tests/find_max_subarray_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 
 class FindMaxSubarrayTest < Minitest::Test
   def test_find_max_crossing_subarray
-    assert_equal(DivideAndConquer::find_max_crossing_subarray([-2, -3, 4, -1, -2, 1, 5, -3], 0, 3, 7), [2, 6, 7])
+    assert_equal(DivideAndConquer::find_max_crossing_subarray([-2, -3, 4, -1, -2, 1, 5, -3], 0, 3, 7), [2, 6, 10])
   end
 
   def test_find_max_subarray
