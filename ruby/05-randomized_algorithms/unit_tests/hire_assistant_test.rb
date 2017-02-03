ff987521a91a06a94a07625880c527390434c2b2
nandukalidindi <nandukalidindi@gmail.com>
2017-02-03 16:16:16 -0500

    Add hire random assistant weird test case :D

--- ruby/05-randomized_algorithms/unit_tests/hire_assistant_test.rb
+++ ruby/05-randomized_algorithms/unit_tests/hire_assistant_test.rb
@@ -7,4 +7,12 @@ class HireAssistantTest < Minitest::Test
   	hired_candidates = RandomizedAlgorithms::hire_assistant(candidates)
   	assert_equal(hired_candidates, [1, 3, 4, 7])
   end
+
+  def test_randomized_hire_assistant
+    candidates = [0, 9, 2, 15, 17, 9, 10, 20]
+    first_run_hired = RandomizedAlgorithms::randomized_hire_assistant(candidates)
+    candidates = [0, 9, 2, 15, 17, 9, 10, 20]
+    second_run_hired = RandomizedAlgorithms::randomized_hire_assistant(candidates)
+    assert(first_run_hired != second_run_hired)
+  end
 end
