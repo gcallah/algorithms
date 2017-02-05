6a09ddd604b013d224d2e0258205b1bc450fa60d
xuzebin <zebinxu7@gmail.com>
2017-02-05 00:13:51 -0500

    change location of test files

--- Java/Heapsort/MinHeapTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# compile and run the unit test
-
-javac -cp ../test/junit-4.12.jar Heap.java MinHeap.java MinHeapTest.java
-java -cp .:../test/junit-4.12.jar:../test/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MinHeapTest
