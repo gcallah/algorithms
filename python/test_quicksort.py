298c77c490cffffa843e25c016561258f04770f1
gcallah <gcallah@mac.com>
2017-02-05 19:26:58 -0500

    Changed quicksort to use random list of random size ten times.

--- python/test_quicksort.py
+++ python/test_quicksort.py
@@ -11,15 +11,16 @@ import quicksort as qs
 class QuicksortTestCase(TestCase):
 
     def test_quick(self):
-        # set up a random list
-        n = random.randint(0, 100)
-        my_l = random.sample(range(1, 1000), n)
-
-        # no return!
-        qs.quicksort(my_l)
-
-        for i in range(0, n - 1):
-            self.assertLessEqual(my_l[i], my_l[i + 1])
+        for j in range(10):
+            # set up a random list
+            n = random.randint(0, 100)
+            my_l = random.sample(range(1, 1000), n)
+    
+            # no return!
+            qs.quicksort(my_l)
+    
+            for i in range(0, n - 1):
+                self.assertLessEqual(my_l[i], my_l[i + 1])
 
 if __name__ == '__main__':
     main()
