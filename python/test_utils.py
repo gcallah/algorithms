951ee6a1212e724516d721c0fd5c92dbb720baf0
gcallah <gcallah@mac.com>
2017-02-06 23:34:10 -0500

    Parameterized range.

--- python/test_utils.py
+++ python/test_utils.py
@@ -5,9 +5,7 @@ Utilities used by test programs.
 import random
 
 
-def rand_list(max_list=20):
+def rand_list(max_list=20, lrange=-1000, hrange=1000):
     # set up a random list of a random length
-    # in the future, the range parameters should be added as parameters to
-    # rand_list() as well.
-    return random.sample(range(-1000, 1000), random.randint(0, max_list))
+    return random.sample(range(lrange, hrange), random.randint(0, max_list))
 
