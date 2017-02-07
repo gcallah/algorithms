f12b806bb7e3f6dc6979f41c65f0cfb251a641a3
gcallah <gcallah@mac.com>
2017-02-06 23:28:42 -0500

    Modified range.

--- python/test_utils.py
+++ python/test_utils.py
@@ -7,5 +7,5 @@ import random
 
 def rand_list(max_list=20):
     # set up a random list
-    return random.sample(range(1, 1000), random.randint(0, max_list))
+    return random.sample(range(-1000, 1000), random.randint(0, max_list))
 
