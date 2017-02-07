90e128ba77a2c75c4a60708b2983bfb17a00fa9d
gcallah <gcallah@mac.com>
2017-02-06 23:30:44 -0500

    changed comment.

--- python/test_utils.py
+++ python/test_utils.py
@@ -6,6 +6,8 @@ import random
 
 
 def rand_list(max_list=20):
-    # set up a random list
+    # set up a random list of a random length
+    # in the future, the range parameters should be added as parameters to
+    # rand_list() as well.
     return random.sample(range(-1000, 1000), random.randint(0, max_list))
 
