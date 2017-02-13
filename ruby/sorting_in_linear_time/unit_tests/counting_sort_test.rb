1f22a24aef5d09aa8c2bed0d8406fe884fb3aac3
nandukalidindi <nandukalidindi@gmail.com>
2017-02-13 11:21:35 -0500

    Add counting sort

--- ruby/sorting_in_linear_time/unit_tests/counting_sort_test.rb
+++ ruby/sorting_in_linear_time/unit_tests/counting_sort_test.rb
@@ -4,11 +4,12 @@ require 'minitest/autorun'
 
 class CountingSortTest < Minitest::Test
   def test_counting_sort
+    sorted_arr = [0, 0, 2, 2, 3, 3, 3, 5]
     a = [2, 5, 3, 0, 2, 3, 0, 3]
     b = []
     k = 5
 
-    SortingInLinearTime::counting_sort(a, b, k)
-    binding.pry
+    SortingInLinearTime::counting_sort(arr, b, 18)
+    assert_equal(b, sorted_arr)
   end
 end
