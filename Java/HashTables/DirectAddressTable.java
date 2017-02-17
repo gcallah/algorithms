20c1be24a953e4654f9f07a39e75384979d13c89
prashant182 <pnp249@nyu.edu>
2017-02-17 00:14:36 -0500

    Added Comments to DIrectAddressTable
    Added Comments to DirectAddessTable

--- Java/HashTables/DirectAddressTable.java
+++ Java/HashTables/DirectAddressTable.java
@@ -1,6 +1,5 @@
 package HashTable;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -10,17 +9,22 @@ public class DirectAddressTable {
     private Item[] table;
     private int capacity;
 
+    //Default Constructor
     public DirectAddressTable() {
     }
 
+    //The table holds the array of class Item
     class Item{
         int key;
         int value;
 
+        //Default Constructor
         public Item(){
             key=0;
             value=0;
         }
+
+        //Parametrized Constructor
         public Item(int key,int value){
             this.key=key;
             this.value=value;
@@ -37,13 +41,15 @@ public class DirectAddressTable {
                     '}';
         }
     }
+    //Get method
     public Item[] getTable() {
         return table;
     }
-
+    //Set method
     public void setTable(Item[] table) {
         this.table = table;
     }
+
     public int getCapacity() {
         return capacity;
     }
@@ -52,21 +58,44 @@ public class DirectAddressTable {
         this.capacity = capacity;
     }
 
+    /**
+    * Note: You might notice that the method signtaure of below methods are different than the one showed in the
+    * book. That is because we are using table (T[]) as a class variable. The essential part logic and implementation
+    * details are same.
+    */
     public DirectAddressTable(int capacity){
         this.capacity=capacity;
         table=new Item[capacity+1];
     }
+
+      /*
+    input:
+        @param1 : Item to be searched
+    output:
+        Item returned
+     */
     public Item DirectAddressSearch(Item x){
         return table[x.key];
     }
 
+    /*
+    input:
+        @param1 : Item to be Insert in the table
+     */
     public void DirectAddressInsert(Item x){
         table[x.key]=x;
     }
+
+    /*
+    input:
+        @param2 : Item to be delete in the table
+     */
     public void DirectAddressDelete(Item x){
         table[x.key]=null;
     }
-
+    /*
+    prints the DirectAddress Table
+     */
     public void printTable(){
         System.out.println("\nDirect Addressing Table : ");
         int l = table.length;
@@ -76,7 +105,7 @@ public class DirectAddressTable {
         System.out.println();
     }
 
-
+    //main method with example input.
     public static void main(String[] args){
         DirectAddressTable directAddressTable =new DirectAddressTable(10);
         Scanner scan = new Scanner(System.in);
@@ -114,6 +143,5 @@ public class DirectAddressTable {
             ch = scan.next().charAt(0);
         } while (ch == 'Y'|| ch == 'y');
     }
-
 
 }
