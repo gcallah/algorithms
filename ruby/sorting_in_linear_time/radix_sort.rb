22e1c19f597079767c9a48e650f76306b4f7c046
nandukalidindi <nandukalidindi@gmail.com>
2017-02-20 01:22:38 -0500

    Add proper radix with bubble and using unstable quick sort

--- ruby/sorting_in_linear_time/radix_sort.rb
+++ ruby/sorting_in_linear_time/radix_sort.rb
@@ -1,38 +1,96 @@
 require 'pry'
+require_relative '../02-getting_started/bubble_sort'
 
 module SortingInLinearTime
   class << self
 
-    # Public: Counting sort assumes that each of the n input elements is an
-    #         integer in the range 0 to k.
+    # Public: Sorts data with integer keys by grouping keys by the individual
+    #         digits which share the same significant position and value.
     #
-    # Visualization link: https://www.cs.usfca.edu/~galles/visualization/CountingSort.html
+    # NOTE: USING STABLE SORT (BUBBLE SORT)
     #
     # ARGS:
     # a - Input array
-    # b - Array that holds the sorted output
-    # k - Maxmimum number in the input array (~ a.max)
+    # d - Highest order digit
     #
-    # RETURN: nil
+    # RETURN: Sorted array
     #
-    # COMPLEXITY: Θ(n)
+    # COMPLEXITY: Θ(w*n)
     #
     # Examples
-    #    counting_sort([2, 5, 3, 0, 2, 3, 0, 3], [], 5)
-    #    => nil
-    #    b = [0, 0, 2, 2, 3, 3, 3, 5]
-    #
-    # Modifies the provided array (i.e b)
+    #    radix_sort([329, 457, 657, 839, 436, 720, 355], 3)
+    #    => [329, 355, 436, 457, 657, 720, 839]
     def radix_sort(arr, d)
+      stable_sortable_array = arr.map { |x| [x, 0] }
+      (0..d-1).reverse_each do |digit|
+        stable_sortable_array = number_ndigit_pair(stable_sortable_array.map { |x| x[0]}, d - 1 - digit)
+        GettingStarted::bubble_sort_for_radix_sort(stable_sortable_array)
+      end
+      stable_sortable_array.map { |x| x[0] }
+    end
+
+    # Public: Builds an array of arrays with first index as the number and
+    #         second index as the digit at specified location from the number
+    #
+    # ARGS:
+    # arr - Input array
+    # digit_offset - Specific digit position (like 10's or 100's)
+    #
+    # RETURN: Sorted array
+    #
+    # Examples
+    #    number_ndigit_pair([329, 457, 657, 839, 436, 720, 355], 0)
+    #    => [[329, 9], [355, 5], [436, 6], [457, 7], [657, 7], [720, 0], [839, 9]]
+    #    0 is the units digit, 1 is the 10's digit and 2 is the 100's digit and so on
+    def number_ndigit_pair(array, digit_offset)
+      pair_array = []
+      array.each do |number|
+        pair_array << [number, (number / 10 ** (digit_offset) % 10)]
+      end
+      pair_array
+    end
+
+    # Public: Sorts data with integer keys by grouping keys by the individual
+    #         digits which share the same significant position and value.
+    #
+    # NOTE: USING UNSTABLE SORT (QUICK SORT)
+    #
+    # ARGS:
+    # a - Input array
+    # d - Highest order digit
+    #
+    # RETURN: Sorted array
+    #
+    # Examples
+    #    radix_sort_unstable([329, 457, 657, 839, 436, 720, 355], 3)
+    #    => [329, 355, 436, 457, 657, 720, 839]
+    def radix_sort_unstable(arr, d)
       arrayified_hash = prepare_number_to_digit_array_map(arr, d).to_a
 
       (0..d-1).reverse_each do |digit|
-        arrayified_hash = arrayified_hash.sort { |x, y| x[1][digit] == y[1][digit] ? x[0] <=> y[0] : x[1][digit] <=> y[1][digit] }
+        arrayified_hash.sort! { |x, y| x[1][digit] == y[1][digit] ?
+                                       x[0] <=> y[0] :
+                                       x[1][digit] <=> y[1][digit]
+                               }
       end
 
       arrayified_hash.map { |x| x[0] }
     end
 
+    # Public: Builds an hash of arrays where first index is the number and
+    #         second index is an array with digits in each element
+    #
+    # ARGS:
+    # a - Input array
+    # d - Highest order digit
+    #
+    # Examples
+    #    prepare_number_to_digit_array_map([329, 457, 657, 839, 436, 720, 355], 3)
+    #    => [
+    #         [329, [3, 2, 9]], [355, [3, 5, 5]], [436, [4, 3, 6]],
+    #         [457, [4, 5, 7]], [657, [6, 5, 7]], [720, [7, 2, 0]],
+    #         [839, [8, 3, 9]]
+    #       ]
     def prepare_number_to_digit_array_map(arr, d)
       hash = {}
       arr.each do |number|
@@ -41,6 +99,16 @@ module SortingInLinearTime
       hash
     end
 
+    # Public: Builds an array with first index as the number and second index as
+    #         the array with elements
+    #
+    # ARGS:
+    # number - Number to be augmented
+    # max_size - Length of second index in the result (OPTIONAL)
+    #
+    # Examples
+    #    number_to_digit_array(329)
+    #    => [329, [3, 2, 9]]
     def number_to_digit_array(number, max_size=0)
       array = []
       while number != 0
--- ruby/sorting_in_linear_time/unit_tests/radix_sort_test.rb
+++ ruby/sorting_in_linear_time/unit_tests/radix_sort_test.rb
@@ -7,4 +7,10 @@ class RadixSortTest < Minitest::Test
     sorted_arr = [329, 355, 436, 457, 657, 720, 839]
     assert_equal(SortingInLinearTime::radix_sort(arr, 3), sorted_arr)
   end
+
+  def test_radix_sort_unstable
+    arr = [329, 457, 657, 839, 436, 720, 355]
+    sorted_arr = [329, 355, 436, 457, 657, 720, 839]
+    assert_equal(SortingInLinearTime::radix_sort_unstable(arr, 3), sorted_arr)
+  end
 end
