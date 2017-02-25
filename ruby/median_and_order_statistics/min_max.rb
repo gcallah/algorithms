541962e9ad4d3800b7f38351ccb16ed788148174
nandukalidindi <nandukalidindi@gmail.com>
2017-02-25 00:41:21 -0500

    Add max and min methods

---
+++ ruby/median_and_order_statistics/min_max.rb
@@ -0,0 +1,24 @@
+module MedianAndOrderStatistics
+  class << self
+    def minimum(a)
+      min = a[0]
+      (1..a.length-1).each do |i|
+        if a[i] < min
+          min = a[i]
+        end
+      end
+      min
+    end
+
+    def maximum
+      max = a[0]
+      (1..a.length-1).each do |i|
+        if a[i] > max
+          max = a[i]
+        end
+      end
+      max
+    end
+
+  end
+end
