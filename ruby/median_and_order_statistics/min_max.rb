4c08bbf722ba76ff7f6e7df2fb0344a5ebde6ada
nandukalidindi <nandukalidindi@gmail.com>
2017-02-25 20:08:30 -0500

    Fix the randomized_select iterative algorithm temporarily by using a usual partition method

--- ruby/median_and_order_statistics/min_max.rb
+++ ruby/median_and_order_statistics/min_max.rb
@@ -1,5 +1,18 @@
 module MedianAndOrderStatistics
   class << self
+    # Public: Returns the minimum number in an array.
+    #
+    # ARGS:
+    # a - Input array
+    #
+    # RETURN: Number
+    # NOTE: Ruby in-built method is array.min
+    #
+    # COMPLEXITY: Θ(n)
+    #
+    # Examples
+    #    minimum([4, 1, 3, 2, 16, 9, 10, 14, 18, 7])
+    #    => 1
     def minimum(a)
       min = a[0]
       (1..a.length-1).each do |i|
@@ -10,6 +23,19 @@ module MedianAndOrderStatistics
       min
     end
 
+    # Public: Returns the maximum number in an array.
+    #
+    # ARGS:
+    # a - Input array
+    #
+    # RETURN: Number
+    # NOTE: Ruby in-built method is array.max
+    #
+    # COMPLEXITY: Θ(n)
+    #
+    # Examples
+    #    maximum([4, 1, 3, 2, 16, 9, 10, 14, 18, 7])
+    #    => 18
     def maximum(a)
       max = a[0]
       (1..a.length-1).each do |i|
--- ruby/median_and_order_statistics/unit_tests/randomized_select_test.rb
+++ ruby/median_and_order_statistics/unit_tests/randomized_select_test.rb
@@ -10,11 +10,11 @@ class RandomizedSelectTest < Minitest::Test
     end
   end
 
-  # def test_randomized_select_iterative
-  #   arr = [4, 1, 3, 2, 16, 9, 10, 14, 18, 7]
-  #   sorted_arr = arr.sort
-  #   (1..sorted_arr.length).each do |i|
-  #     assert_equal(MedianAndOrderStatistics::randomized_select_iterative(arr, 0, 9, i), sorted_arr[i-1])
-  #   end
-  # end
+  def test_randomized_select_iterative
+    arr = [4, 1, 3, 2, 16, 9, 10, 14, 18, 7]
+    sorted_arr = arr.sort
+    (1..sorted_arr.length).each do |i|
+      assert_equal(MedianAndOrderStatistics::randomized_select_iterative(arr, 0, 9, i), sorted_arr[i-1])
+    end
+  end
 end
