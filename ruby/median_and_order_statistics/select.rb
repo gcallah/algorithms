e6c24dde49d811776bea8ab55677e9447c3f0372
nandukalidindi <nandukalidindi@gmail.com>
2017-02-28 11:00:33 -0500

    Add some stub methods as explained pointwise in the textbook

--- ruby/median_and_order_statistics/select.rb
+++ ruby/median_and_order_statistics/select.rb
@@ -1,7 +1,22 @@
 module MedianAndOrderStatistics
   class << self
     def select
-      
+      if i == k
+        return x
+      elsif i < k
+        
+      else
+      end
+
     end
+
+    def divide_into_5_groups
+
+    end
+
+    def median_using_insertion_sort
+    end
+
+    def
   end
 end
