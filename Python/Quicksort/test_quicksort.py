e69bfe21851da6d00e1dd6e80df832d41d7e3e7f
nandukalidindi <nandukalidindi@gmail.com>
2017-03-13 19:57:48 -0400

    Relative and lib imports for heapsort and quicksort tests

--- Python/Quicksort/test_quicksort.py
+++ Python/Quicksort/test_quicksort.py
@@ -4,8 +4,8 @@ Test our sorting code.
 """
 
 from unittest import TestCase, main
-from quicksort import quicksort
-from test_utils import rand_list
+from .quicksort import quicksort
+from lib.test_utils import rand_list
 
 
 class QuicksortTestCase(TestCase):
@@ -13,10 +13,10 @@ class QuicksortTestCase(TestCase):
     def test_quicksort(self):
         for j in range(10):
             l = rand_list(max_list=100)
-    
+
             # no return!
             quicksort(l)
-    
+
             for i in range(0, len(l) - 1):
                 self.assertLessEqual(l[i], l[i + 1])
 
