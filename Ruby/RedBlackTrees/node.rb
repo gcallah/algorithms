a834582c52acbbf22dbb68947f48897d955d8b5b
nandukalidindi <nandukalidindi@gmail.com>
2017-07-02 21:18:01 -0400

    Left and right rotate in mofules

--- Ruby/RedBlackTrees/node.rb
+++ Ruby/RedBlackTrees/node.rb
@@ -1,12 +1,14 @@
-class Node
-  attr_accessor :key, :satellite_data, :p, :left, :right
-  def initialize(key, satellite_data, p, left, right)
-    @key, @color, @p, @left, @right = key, satellite_data, p, left, right
-  end
+module RedBlackTrees
+  class Node
+    attr_accessor :key, :satellite_data, :color, :p, :left, :right
+    def initialize(key, satellite_data, color, p, left, right)
+      @key, @satellite_data, @color, @p, @left, @right = key, satellite_data, color, p, left, right
+    end
 
-  #   Node.new(10, nil, nil, nil)
-  #   # => #<Node:0x007ffeab2187a0 @key=10, @p=nil, @left=nil, @right=nil>
-  def initialize(key, color, p, left, right)
-    @key, @color, @p, @left, @right = key, color, p, left, right
+    #   Node.new(10, nil, nil, nil)
+    #   # => #<Node:0x007ffeab2187a0 @key=10, @p=nil, @left=nil, @right=nil>
+    def initialize(key, color, p, left, right)
+      @key, @color, @p, @left, @right = key, color, p, left, right
+    end
   end
 end
--- Ruby/RedBlackTrees/rotations.rb
+++ Ruby/RedBlackTrees/rotations.rb
@@ -1,25 +1,50 @@
-def left_rotate(t, x)
-  y = x.right
-  x.right = y.left
+module RedBlackTrees
+  module Rotations
+    def self.left_rotate(t, x)
+      return if x.right == t.nil
 
-  if y.left != t.nil
-    y.left.p = x
+      y = x.right
+      x.right = y.left
+
+      if y.left != t.nil
+        y.left.p = x
+      end
+
+      y.p = x.p
+
+      if x.p == t.nil
+        t.root = y
+      elsif x == x.p.left
+        x.p.left = y
+      else
+        x.p.right = y
+      end
+
+      y.left = x
+      x.p = y
+    end
+
+    def right_rotate(t, x)
+      return if x.left == t.nil
+
+      y = x.left
+      x.left = y.right
+      if y.right != t.nil
+        y.right.p = x
+      end
+
+      y.p = x.p
+
+      if x.p == t.nil
+        t.root = y
+      elsif x == x.p.right
+        x.p.right = y
+      else
+        x.p.left = y
+      end
+
+      y.right = x
+      x.p = y
+    end
   end
-
-  y.p = x.p
-
-  if x.p == t.nil
-    t.root = y
-  elsif x == x.p.left
-    x.p.left = y
-  else
-    x.p.right = y
-  end
-
-  y.left = x
-  x.p = y
-end
-
-def right_rotate(t, x)
-
 end
