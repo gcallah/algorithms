950e529b6a648d6ecf69b44aa38edc4a3724b08a
gcallah <gcallah@mac.com>
2017-09-20 15:12:48 -0400

    Added some detail to interface.

--- Algocynfas/test_lists.js
+++ Algocynfas/test_lists.js
@@ -2,10 +2,22 @@
  * Created by jaildar on 15/08/17.
  */
 
+const LEFT = 0;
+const RIGHT = 1;
+
 var height=parseInt(document.getElementById("canvas").getAttribute("height"));
 var width=parseInt(document.getElementById("canvas").getAttribute("width"));
 
-function addElem(value)
+function addRowElem(value, next_to = null, place = RIGHT)
+{
+    /* If first elem, place in center. If not, we need LEFT or RIGHT. */
+}
+
+function colorElem(value)
+{
+}
+
+function createList()
 {
 }
 
