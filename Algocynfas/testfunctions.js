e6d87a5f07b735082b92e8eb5fcf33a72f74c1d8
Varun Chopra <vc1202@nyu.edu>
2017-09-23 17:35:59 -0400

    Fixed the bug which was not allowing the edges to be rendered

--- Algocynfas/testfunctions.js
+++ Algocynfas/testfunctions.js
@@ -1,46 +1,110 @@
 /**
- * Created by jaildar on 15/08/17.
+ * Created by Varun on 15/08/17.
  */
+var height=parseInt(document.getElementById("canvas").getAttribute("height"));
+var width=parseInt(document.getElementById("canvas").getAttribute("width"));
+var distance=200;
+var radius=20;
+var linelimit=180;
 
- var height=parseInt(document.getElementById("canvas").getAttribute("height"));
- var width=parseInt(document.getElementById("canvas").getAttribute("width"));
+function addline(x1,y1,x2,y2)
+{
+    var line=new fabric.Line([x1,y1,x2,y2],{stroke:'black',originX:'center',originY:'center'});
+    canvas.add(line);
+
+
+}
 function addRoot(value)
 {
-    var x=new fabric.Circle({ radius: 20,originX: 'center', originY: 'center',fill:'red'});
-    var value1=String(value);
-    var text= new fabric.Text(value1, {fontSize: 10, originX: 'center', originY: 'center'});
-    var group = new fabric.Group([ x, text ], {left:width/2, top: 10, angle:0});
-    canvas.add(group);
-    return group;
+    var value=String(value);
+    var x=new fabric.Circle({ radius: radius,left:width/2,fill:'red',top:radius,originX: 'center', originY: 'center',fill:'red'});
+    var text= new fabric.Text(value, {fontSize: 10, originX: 'center', originY: 'center',left:width/2,top:radius});
+    // var group=new fabric.Group([x,text],{originX:'center',originY:'center',left:width/2,top:radius});
+    canvas.add(x);
+    canvas.add(text);
+    return x;
+
 
 }
 
 function addNode(node1,node2,angle)
 {
     var intialx=parseInt(node1.get('left'));
+
     var initialy=parseInt(node1.get('top'));
 
-    if(angle<180)
+    if(angle>0 && angle <=90)
     {
 
 
-        var pointx =Math.abs(Math.cos(angle*Math.PI/180)*200+intialx);
-        var pointy=Math.abs(Math.sin(angle*Math.PI/180)*200-initialy);
+        var pointx =Math.abs(Math.abs(Math.cos(angle*Math.PI/180)*distance)+intialx);
+        var pointy=Math.abs(Math.abs(Math.sin(angle*Math.PI/180)*distance)-initialy);
+
+        var initiallinex=Math.abs(Math.abs(Math.cos(angle*Math.PI/180)*radius)+intialx);
+        var initialliney=Math.abs(Math.abs(Math.sin(angle*Math.PI/180)*radius)-initialy);
+        var finallinex=Math.abs(Math.abs(Math.cos(angle*Math.PI/180)*linelimit)+intialx);
+        var finalliney=Math.abs(Math.abs(Math.sin(angle*Math.PI/180)*linelimit)-initialy);
+
+    }
+    if(angle>90&&angle<=180)
+    {
+        var pointx =Math.abs(Math.cos(angle*Math.PI/180)*distance+intialx);
+        var pointy=Math.abs(Math.sin(angle*Math.PI/180)*distance-initialy);
+
+        var initiallinex=Math.abs(Math.cos(angle*Math.PI/180)*radius+intialx);
+        var initialliney=Math.abs(Math.sin(angle*Math.PI/180)*radius-initialy);
+        var finallinex=Math.abs(Math.cos(angle*Math.PI/180)*linelimit+intialx);
+        var finalliney=Math.abs(Math.sin(angle*Math.PI/180)*linelimit-initialy);
+
+
+    }
+    if(angle>180&&angle<=270)
+    {
+        if (angle==270)
+        {
+            var pointx =Math.abs(Math.cos(angle*Math.PI/180)*distance+intialx);
+            var pointy=Math.abs(Math.sin(angle*Math.PI/180)*distance-initialy);
+            var initiallinex=Math.abs(Math.cos(angle*Math.PI/180)*radius+intialx);
+            var initialliney=Math.abs(Math.sin(angle*Math.PI/180)*radius-initialy);
+            var finallinex=Math.abs(Math.cos(angle*Math.PI/180)*linelimit+intialx);
+            var finalliney=Math.abs(Math.sin(angle*Math.PI/180)*linelimit-initialy);
+        }
+        else {
+            var pointx = Math.abs(Math.cos(angle * Math.PI / 180) * distance + intialx);
+            var pointy = Math.abs(Math.sin(angle * Math.PI / 180) * distance - initialy);
+            var initiallinex=Math.abs(Math.cos(angle*Math.PI/180)*radius+intialx);
+            var initialliney=Math.abs(Math.sin(angle*Math.PI/180)*radius-initialy);
+            var finallinex=Math.abs(Math.cos(angle*Math.PI/180)*linelimit+intialx);
+            var finalliney=Math.abs(Math.sin(angle*Math.PI/180)*linelimit-initialy);
+        }
+
 
     }
     else
     {
 
-        var pointx =Math.abs(Math.cos(angle*Math.PI/180)*200+intialx);
-        var pointy=Math.abs(Math.sin(angle*Math.PI/180)*200+initialy);
+        var pointx =Math.abs(Math.cos(angle*Math.PI/180)*distance+intialx);
+        var pointy=Math.abs(Math.sin(angle*Math.PI/180)*distance-initialy);
+        var initiallinex=Math.abs(Math.cos(angle*Math.PI/180)*radius+intialx);
+        var initialliney=Math.abs(Math.sin(angle*Math.PI/180)*radius-initialy);
+        var finallinex=Math.abs(Math.cos(angle*Math.PI/180)*linelimit+intialx);
+        var finalliney=Math.abs(Math.sin(angle*Math.PI/180)*linelimit-initialy);
+
     }
 
-    var x=new fabric.Circle({ radius: 20,originX: 'center', originY: 'center',fill:'red'});
+
+    var x=new fabric.Circle({ radius:radius,originX: 'center', originY: 'center',fill:'red',left:pointx,top:pointy});
+    console.log(initiallinex);
+    console.log(initialliney);
+    console.log(finallinex);
+    console.log(finalliney);
+    addline(initiallinex,initialliney,finallinex,finalliney);
     var value1=String(node2);
-    var text= new fabric.Text(value1, {fontSize: 10, originX: 'center', originY: 'center'});
-    var group = new fabric.Group([ x, text ], {left:pointx, top: pointy, angle:0});
-    canvas.add(group);
-    return group;
+    var text= new fabric.Text(value1, {fontSize: 10, originX: 'center', originY: 'center',left:pointx,top:pointy});
+    canvas.add(x);
+    canvas.add(text);
+
+    return x
 
 
 
