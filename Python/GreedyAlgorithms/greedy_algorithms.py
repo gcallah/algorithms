f07085e6624c34832ae17f910be24558eae50050
gcallah <gcallah@mac.com>
2017-10-16 16:49:11 -0400

    HTML rebuild.

--- Python/GreedyAlgorithms/greedy_algorithms.py
+++ Python/GreedyAlgorithms/greedy_algorithms.py
@@ -15,6 +15,8 @@ This file contains:
     greedy_activity_selector()
 """
 
+start = [1,  3, 0, 5, 3, 5, 6, 8, 8, 2, 12]
+finish = [4,  5, 6, 7, 9, 9, 10, 11, 12, 14, 16]
 
 def recursive_activity_selector(s, f, k, n=None):
     """
